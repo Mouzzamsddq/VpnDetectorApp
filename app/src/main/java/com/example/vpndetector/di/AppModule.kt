84ebaa225b4e1d8c41c882766189bc2baa03bec6
MouzzamSiddiqui07 <mouzzam.siddiqui@zopsmart.com>
2023-08-13 18:55:22 +0530

    #3 added api service in hilt module

--- app/src/main/java/com/example/vpndetector/di/AppModule.kt
+++ app/src/main/java/com/example/vpndetector/di/AppModule.kt
@@ -1,11 +1,16 @@
 package com.example.vpndetector.di
 
+import com.example.vpndetector.data.remote.post.PostApiService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
+    @Provides
+    fun providePostApiService(retrofit: Retrofit): PostApiService =
+        retrofit.create(PostApiService::class.java)
 }
